de36ef7e34d41bd3d9d1e4dcc5cf165a1fb039d5
artur <ta01rus@gmail.com>
2023-07-30 12:38:17 +0300

    (savve)

--- pkg/storage/interface.go
+++ pkg/storage/interface.go
@@ -3,17 +3,12 @@ package storage
 import "context"
 
 type DB interface {
-	Task(ctx context.Context, id int) (*TaskView, error)
-	// получение заданий
 	Tasks(ctx context.Context, id, athID, asgID int, offset, limit int) ([]*TaskView, error)
-
 	AddTasks(context.Context, *TaskView) (*TaskView, error)
-
 	DelTasks(context.Context, int) error
 
 	// пользователи
 	Users(ctx context.Context, id int, offset, limit int) ([]*Users, error)
-
 	// добаавить пользователя
 	AddUsers(context.Context, *Users) (*Users, error)
 
